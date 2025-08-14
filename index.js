478f781863759f46f2ee6acf0c3ffcda23c0f665
ghibhs <gabrlz@outlook.com>
2025-08-14 11:53:45 -0300

    aula 14/08 (incompleto)

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 import express from 'express'
 
-const app = express()
-const PORT = 3000
+const app = express();
+const PORT = 3000;
 
-app.use(express.json())
+let usuarios = [];
+let contador = 1;
+
+app.use(express.json());
 
 //ROTA DE TESTE
 app.get("/status", (req, res) => {
-  res.json({message: "API Online"})
+  res.json({message: "API Online"});
+});
+
+app.get("/usuarios", (req, res) => {
+  res.json(usuarios);
+});
+
+app.post("/usuarios", (req, res) => {
+  
 })
 
 app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`)
-})
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
